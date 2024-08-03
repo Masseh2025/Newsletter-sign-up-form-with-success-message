8e39b6c56a0a965f63b24e5a6c7a62fd5b88803c
mas <mas154572@gmail.com>
2024-08-02 23:05:53 -0400

    finshed  form error checkness

--- newsletter-sign-up-with-success-message-main/script.js
+++ newsletter-sign-up-with-success-message-main/script.js
@@ -1,1 +1,18 @@
+const email = document.getElementById("email");
+const button = document.getElementById("button");
+const form = document.getElementById("form");
+const errorMessage = document.getElementById("error");
 
+form.addEventListener("submit", function (e) {
+  if (
+    /^[-a-z0-9~!$%^&*_=+}{\'?]+(\.[-a-z0-9~!$%^&*_=+}{\'?]+)*@([a-z0-9_][-a-z0-9_]*(\.[-a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/i.test(
+      email.value
+    )
+  ) {
+    null;
+  } else {
+    e.preventDefault();
+    errorMessage.classList.add("visible");
+    email.classList.add("red");
+  }
+});
